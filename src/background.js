de20b3902ef6bd638955c51460455a455fd3671a
jurakovic <17744091+jurakovic@users.noreply.github.com>
2024-07-30 08:52:31 +0200

    check url pattern

--- src/background.js
+++ src/background.js
@@ -1,42 +1,48 @@
-//src: https://stackoverflow.com/a/67729185
+
+function changeBranchesHref() {
+	//console.log("page loaded")
+
+	currentLoc = document.location.href.substring(18)
+	//console.log(currentLoc)
+	nodeList = document.querySelectorAll(`a[href='${currentLoc}/branches']`)
+	//console.log(nodeList)
+
+	nodeList.forEach(anchor => {
+		anchor.href = `${anchor.href}/all`
+		//console.log(anchor.href)
+	});
+
+	console.log("branches href changed")
+}
 
 // function that injects code to a specific tab
 function injectScript(tabId) {
-    chrome.scripting.executeScript(
-        {
-            target: { tabId: tabId },
-            function: changeBranchesHref,
-        }
-    );
+	chrome.scripting.executeScript(
+		{
+			target: { tabId: tabId },
+			function: changeBranchesHref,
+		}
+	);
 }
 
 // adds a listener to tab change
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 
-    if (tab.url?.startsWith("edge://")) return undefined;
-    if (!tab.url?.startsWith("https://github.com")) return undefined;
+	const urlPattern = /^https:\/\/github\.com\/[a-zA-Z0-9-]+\/[a-zA-Z0-9-]+\/?$/;
 
-    // check for a URL in the changeInfo parameter (url is only added when it is changed)
-    //if (changeInfo.url) {
-    if (changeInfo.status === 'complete') {
-        injectScript(tabId);
-    }
+	if (urlPattern.test(tab.url)) {
+		//console.log('matched url');
 
-    //console.log(changeInfo);
+		//if (changeInfo.url) {
+		if (changeInfo.status === 'complete') {
+			//console.log('injecting script');
+			injectScript(tabId);
+		}
+
+		//console.log(changeInfo);
+	}
+	//else
+	//{
+	//	console.log('not matched url');
+	//}
 });
-
-function changeBranchesHref() {
-    //console.log("page loaded")
-
-    currentLoc = document.location.href.substring(18)
-    //console.log(currentLoc)
-    nodeList = document.querySelectorAll(`a[href='${currentLoc}/branches']`)
-    //console.log(nodeList)
-
-    nodeList.forEach(anchor => {
-        anchor.href = `${anchor.href}/all`
-        //console.log(anchor.href)
-    });
-
-    console.log("branches href changed")
-}
