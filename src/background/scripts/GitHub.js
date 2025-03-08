7eb7235799a825fbca93fdc88e72fcc5c66002c5
jurakovic <17744091+jurakovic@users.noreply.github.com>
2025-03-08 21:56:22 +0100

    test page logger

--- src/background/scripts/GitHub.js
+++ src/background/scripts/GitHub.js
@@ -1,30 +1,21 @@
+import { log } from '../../utils/logger.js';
 
 export function checkGitHub(tabId, changeInfo, tab, flagEnabled) {
 	const urlPattern = /^https:\/\/github\.com\/[a-zA-Z0-9-\.]+\/[a-zA-Z0-9-\.]+\/?/;
 
 	if (flagEnabled && urlPattern.test(tab.url)) {
-		//console.log('matched url');
+		log('matched url');
 
-		//if (changeInfo.url) {
 		if (changeInfo.status === 'complete') {
-			//console.log('injecting script');
+			log('injecting script');
 			injectScriptGitHub(tabId);
 		}
 
-		//console.log(changeInfo);
+		log(changeInfo);
 	}
-	//else
-	//{
-	//	console.log('not matched url');
-	//}
 };
 
 function injectScriptGitHub(tabId) {
-	//if (!flagEnabled) {
-	//	console.log('Feature is disabled. Not injecting script.');
-	//	return;
-	//}
-
 	chrome.scripting.executeScript(
 		{
 			target: { tabId: tabId },
@@ -34,31 +25,46 @@ function injectScriptGitHub(tabId) {
 }
 
 function changeBranchesHref() {
-	//console.log("page loaded")
+	function log(message, data) {
+		//chrome.runtime.sendMessage({ type: "log", level: "info", message });
+		chrome.runtime.sendMessage({
+			type: "log",
+			level: "info",
+			message: message,
+			data: data // Send additional data as an object
+			//message: typeof message === "string" ? message : "[OBJECT LOG]",
+			//data: typeof message === "object" ? message : null
+		});
+	}
+
+	log("page loaded")
 
 	rx = /^https:\/\/github\.com\/([a-zA-Z0-9-\.]+)\/([a-zA-Z0-9-\.]+)\/?/
 	match = rx.exec(document.location)
 	user = match[1]
 	repo = match[2]
-	//console.log(user)
-	//console.log(repo)
+	log(user)
+	log(repo)
 
 	nodeList = document.querySelectorAll(`a[href='/${user}/${repo}/branches']`)
-	//console.log(nodeList)
-	//console.log(nodeList.length)
+	console.log(nodeList)
+	log(nodeList)
+	//log("asd", nodeList)
+	log("asd", JSON.stringify(nodeList, null, 2))
+	log(nodeList.length)
 
 	if (nodeList.length > 0) {
-		//console.log("has nodes")
+		log("has nodes")
 
 		nodeList.forEach(anchor => {
 			anchor.href = `${anchor.href}/all`
-			//console.log(anchor.href)
+			log(anchor.href)
 		});
 
-		console.log("branches href changed")
+		log("branches href changed")
 	}
-	//else
-	//{
-	//	console.log('has no nodes');
-	//}
+	else
+	{
+		log('has no nodes');
+	}
 }
