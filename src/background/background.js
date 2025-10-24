fe53e20008705e9f91e8091b885e1e19ea1e4c1f
jurakovic <17744091+jurakovic@users.noreply.github.com>
2025-10-24 22:38:58 +0200

    formatting

--- src/background/background.js
+++ src/background/background.js
@@ -5,10 +5,10 @@ let flagEnabled = false;
 
 // Function to update the flag value when it changes
 function updateFlagValue() {
-	chrome.storage.sync.get(['flagEnabled'], (result) => {
-		flagEnabled = result.flagEnabled || false; // Default to false if not set
-		console.log('Flag value updated:', flagEnabled);
-	});
+    chrome.storage.sync.get(['flagEnabled'], (result) => {
+        flagEnabled = result.flagEnabled || false; // Default to false if not set
+        console.log('Flag value updated:', flagEnabled);
+    });
 }
 
 // Call updateFlagValue initially to load the current flag state
@@ -16,13 +16,13 @@ updateFlagValue();
 
 // Listen for changes in the storage to update the flag dynamically
 chrome.storage.onChanged.addListener((changes, areaName) => {
-	if (areaName === 'sync' && changes.flagEnabled) {
-		flagEnabled = changes.flagEnabled.newValue;
-		console.log('Flag value changed:', flagEnabled);
-	}
+    if (areaName === 'sync' && changes.flagEnabled) {
+        flagEnabled = changes.flagEnabled.newValue;
+        console.log('Flag value changed:', flagEnabled);
+    }
 });
 
 // adds a listener to tab change
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-	checkGitHub(tabId, changeInfo, tab, flagEnabled);
+    checkGitHub(tabId, changeInfo, tab, flagEnabled);
 });
--- src/background/scripts/github.js
+++ src/background/scripts/github.js
@@ -1,55 +1,54 @@
 import { log } from '../../utils/logger.js';
 
 export function checkGitHub(tabId, changeInfo, tab, flagEnabled) {
-	const urlPattern = /^https:\/\/github\.com\/([a-zA-Z0-9-_\.]+)\/([a-zA-Z0-9-_\.]+)\/?/;
+    const urlPattern = /^https:\/\/github\.com\/([a-zA-Z0-9-_\.]+)\/([a-zA-Z0-9-_\.]+)\/?/;
 
-	if (flagEnabled && urlPattern.test(tab.url)) {
-		log('matched url');
+    if (flagEnabled && urlPattern.test(tab.url)) {
+        log('matched url');
 
-		if (changeInfo.status === 'complete') {
-			log('injecting script');
-			injectScriptGitHub(tabId);
-		}
+        if (changeInfo.status === 'complete') {
+            log('injecting script');
+            injectScriptGitHub(tabId);
+        }
 
-		log(changeInfo);
-	}
+        log(changeInfo);
+    }
 };
 
 function injectScriptGitHub(tabId) {
-	chrome.scripting.executeScript(
-		{
-			target: { tabId: tabId },
-			function: changeBranchesHref,
-		}
-	);
+    chrome.scripting.executeScript(
+        {
+            target: { tabId: tabId },
+            function: changeBranchesHref,
+        }
+    );
 }
 
 function changeBranchesHref() {
-	console.log("page loaded")
+    console.log("page loaded")
 
-	rx = /^https:\/\/github\.com\/([a-zA-Z0-9-_\.]+)\/([a-zA-Z0-9-_\.]+)\/?/
-	match = rx.exec(document.location)
-	user = match[1]
-	repo = match[2]
-	console.log(user)
-	console.log(repo)
+    rx = /^https:\/\/github\.com\/([a-zA-Z0-9-_\.]+)\/([a-zA-Z0-9-_\.]+)\/?/
+    match = rx.exec(document.location)
+    user = match[1]
+    repo = match[2]
+    console.log(user)
+    console.log(repo)
 
-	nodeList = document.querySelectorAll(`a[href='/${user}/${repo}/branches']`)
-	console.log(nodeList)
-	console.log(nodeList.length)
+    nodeList = document.querySelectorAll(`a[href='/${user}/${repo}/branches']`)
+    console.log(nodeList)
+    console.log(nodeList.length)
 
-	if (nodeList.length > 0) {
-		console.log("has nodes")
+    if (nodeList.length > 0) {
+        console.log("has nodes")
 
-		nodeList.forEach(anchor => {
-			anchor.href = `${anchor.href}/all`
-			console.log(anchor.href)
-		});
+        nodeList.forEach(anchor => {
+            anchor.href = `${anchor.href}/all`
+            console.log(anchor.href)
+        });
 
-		console.log("branches href changed")
-	}
-	else
-	{
-		console.log('has no nodes');
-	}
+        console.log("branches href changed")
+    }
+    else {
+        console.log('has no nodes');
+    }
 }
