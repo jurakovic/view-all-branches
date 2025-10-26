3a389d16e4217702963333f2bdeeefb0eafa89f5
jurakovic <17744091+jurakovic@users.noreply.github.com>
2025-10-26 22:54:11 +0100

    update href for all repos (in top navigation)

--- src/background/scripts/azuredevops.js
+++ src/background/scripts/azuredevops.js
@@ -46,41 +46,33 @@ function changeBranchesHref(loggingEnabled) {
 
     log("Page loaded, setting up link observer.");
 
-    const rx = /^https:\/\/dev\.azure\.com\/(?<user>[a-zA-Z0-9%-\.]+(?:\/*)[a-zA-Z0-9%-\.]+)?(?:\/_git)\/([a-zA-Z0-9%-\.]+)/
-    const match = rx.exec(document.location);
-    if (!match) return;
-
-    const proj = match[1];
-    const repo = match[2];
-    log(`User: ${proj}`);
-    log(`Repo: ${repo}`);
-    const branchesHref = `/${proj}/_git/${repo}/branches`;
-
     // This function finds and updates the branch links
     const updateBranchLinks = () => {
-        // Select links that have the exact href and have not been updated yet
-        const nodeList = document.querySelectorAll(`a[href='${branchesHref}']`);
+        // Select any link that points to a branches page for any repo, but has not been updated yet.
+        const nodeList = document.querySelectorAll("a[href*='/_git/'][href$='/branches']");
 
         if (nodeList.length > 0) {
             log(`Found and updated ${nodeList.length} new branch link(s).`);
 
             nodeList.forEach(anchor => {
+                // Prevent Azure DevOps's SPA routing from interfering
                 anchor.addEventListener('click', function (event) {
-                    event.preventDefault(); // prevent azure devops from handling the click
+                    event.preventDefault();
                     window.location.href = anchor.href;
                 });
 
-                anchor.href = `${anchor.href}?_a=all`
+                // Update the href to point to the "All" branches tab
+                anchor.href = `${anchor.href}?_a=all`;
+
                 // Apply styles for debugging
-                anchor.style.color = 'red';
-                anchor.style.fontWeight = 'bold';
+                //.style.color = 'red';
+                //anchor.style.fontWeight = 'bold';
             });
         }
     };
 
     // Create an observer to watch for DOM changes
     const observer = new MutationObserver((mutations) => {
-        // We can debounce this if it becomes a performance issue, but for now, it's fine.
         updateBranchLinks();
     });
 
